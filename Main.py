1ac839a6208de0b1c6a0404e3babe875d0b66f4a
mmcginn1 <matthew.mcginn96@gmail.com>
2018-06-14 21:53:05 -0400

    made fft work

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 from scipy.io import wavfile
 import numpy
-<<<<<<< HEAD
-#Project Created
-fs, data = wavfile.read('note.wav')
-#otp = numpy.fft(data)
-#print(otp)
 
-def main():
-    print("This is a test")
+#Project Created
+
+fs, data = wavfile.read('note.wav')
+otp = numpy.fft.fft(data)
+print(otp)
+
+#def main():
+#    print("This is a test")
