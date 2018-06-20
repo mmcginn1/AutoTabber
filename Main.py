7162c92ec7f19bca29976ec50fa0b11f0f71d87b
mmcginn1 <matthew.mcginn96@gmail.com>
2018-06-19 20:52:13 -0400

    fixed fft issues and have amp v freq plot

--- Main.py
+++ Main.py
@@ -1,12 +1,19 @@
-from readWav import readWavFile
-from fft import fft
+from scipy.io import wavfile
+from scipy.fftpack import fft
+from matplotlib import pyplot as plt
+#import numpy
 
-#driver function
-def main():
-    filename = input("Input filename: ")
-    data = readWavFile(filename)
-    freqData = fft(data)
-    for x in range(0, 100):
-        print(freqData[x])
+#Project Created
 
-main()
+fs, data = wavfile.read('note.wav')
+
+a = data.T[0]  # this is a two channel soundtrack, I get the first track
+b = [(ele/2**8.)*2-1 for ele in a]  # this is 8-bit track, b is now normalized on [-1,1)
+otp = fft(b)  # calculate fourier transform (complex numbers list)
+d = len(otp)/2  # you only need half of the fft list (real signal symmetry)
+plt.plot(abs(otp), 'r')
+plt.show()
+
+
+#def main():
+#    print("This is a test")
